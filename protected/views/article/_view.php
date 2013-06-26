b5519f69428806d656ad1e8eeae40968db49b7a9
Talhaakkas <Talhaakkas@gmail.com>
2013-06-26 20:28:09 +0300

    off

--- protected/views/article/_view.php
+++ protected/views/article/_view.php
@@ -12,7 +12,7 @@ if ($data->holder0) {
     <h1><?php //echo CHtml::encode($data->category);    ?></h1>
     <br />
 
-    <h1><?php echo CHtml::link(CHtml::encode($data->title), "../article/" . $data->id, $options = array('class' => 'blacklink')); ?></h1>
+    <h3><?php echo CHtml::link(CHtml::encode($data->title), "../article/" . $data->id, $options = array('class' => 'blacklink')); ?></h3>
     <input rows="6" cols="50" name="Article[tagstr]" id="<?php echo $data->id ?>dArticle_tagstr" type="hidden" >
     <div id="<?php echo $data->id ?>dynamicInput"></div>
     <script type="text/javascript">addTagsLinkedOnload("<?php echo $usingTags; ?>",'<?php echo $data->id ?>dynamicInput','<?php echo $data->id ?>dArticle_tagstr', '<?php echo Yii::app()->request->baseUrl ?>');</script>
